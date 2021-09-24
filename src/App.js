2b3d0fa2a5f5667ebeb5e217c2024cbf61ece8f0
Greg <gbrewton1980@gmail.com>
2021-09-23 21:05:08 -0600

    Connects a better premade action

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ import TotalDisplay from "./components/TotalDisplay";
 import CalcButton from "./components/CalcButton";
 import reducer, { initialState } from "./reducers/index";
 
-import { addOne } from "./actions";
+// import { addOne, applyNumber } from "./actions";
+import { applyNumber } from "./actions";
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handle1Click = () => {
-    dispatch(addOne());
+  // const handle1Click = () => {
+  //   dispatch(addOne());
+  // };
+
+  const handleNumClick = (e) => {
+    // e.target.value will not work here
+    const { innerText } = e.target;
+    dispatch(applyNumber(innerText));
   };
 
   return (
@@ -47,27 +54,28 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={handle1Click} value={1} />
-              <CalcButton value={2} />
-              <CalcButton value={3} />
+              {/* <CalcButton onClick={handle1Click} value={1} /> */}
+              <CalcButton onClick={handleNumClick} value={1} />
+              <CalcButton onClick={handleNumClick} value={2} />
+              <CalcButton onClick={handleNumClick} value={3} />
             </div>
 
             <div className="row">
-              <CalcButton value={4} />
-              <CalcButton value={5} />
-              <CalcButton value={6} />
+              <CalcButton onClick={handleNumClick} value={4} />
+              <CalcButton onClick={handleNumClick} value={5} />
+              <CalcButton onClick={handleNumClick} value={6} />
             </div>
 
             <div className="row">
-              <CalcButton value={7} />
-              <CalcButton value={8} />
-              <CalcButton value={9} />
+              <CalcButton onClick={handleNumClick} value={7} />
+              <CalcButton onClick={handleNumClick} value={8} />
+              <CalcButton onClick={handleNumClick} value={9} />
             </div>
 
             <div className="row">
-              <CalcButton value={"+"} />
-              <CalcButton value={"*"} />
-              <CalcButton value={"-"} />
+              <CalcButton onClick={handleNumClick} value={"+"} />
+              <CalcButton onClick={handleNumClick} value={"*"} />
+              <CalcButton onClick={handleNumClick} value={"-"} />
             </div>
 
             <div className="row ce_button">
