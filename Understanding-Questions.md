e84963fa79bbc4d5db6cd45d662dbb3b0b66eaba
Greg <gbrewton1980@gmail.com>
2021-09-23 20:52:22 -0600

    Connects a premade action

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,12 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
+* The user presses the `1` button.
+* Click event is fired, `handle1Click` is called
+* Inside `handle1Click`, `dispatch` is called
+* Parameters for `dispatch` are evaluated and `addOne` is called, returning an action object to be used by `dispatch`
+* `ADD_ONE` action is dispatched
+* `useReducer` hook uses that dispatch to make changes to state ala the `reducer` function
+* a new state object is returned by `useReducer` and evaluated against the previous state to update the component
 ...
 
-* TotalDisplay shows the total plus 1.
+* `TotalDisplay` shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import "./App.css";
 
 import TotalDisplay from "./components/TotalDisplay";
 import CalcButton from "./components/CalcButton";
-import reducer, {initialState} from './reducers/index';
+import reducer, { initialState } from "./reducers/index";
+
+import { addOne } from "./actions";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const handle1Click = () => {
+    dispatch(addOne());
+  };
 
   return (
     <div className="App">
@@ -42,7 +47,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
+              <CalcButton onClick={handle1Click} value={1} />
               <CalcButton value={2} />
               <CalcButton value={3} />
             </div>
